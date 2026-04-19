e6da74cc0ca2393c11ba2f8b1f4d5b532a70a416
Ayush <giriayush624@gmail.com>
2026-04-19 16:57:21 +0530

    fix cors

--- next.config.js
+++ next.config.js
@@ -5,7 +5,7 @@ const nextConfig = {
       {
         source: "/api/:path*",
         headers: [
-          { key: "Access-Control-Allow-Origin", value: "*" },
+          { key: "Access-Control-Allow-Origin", value: "https://code-reviewer-app-seven.vercel.app" },
           { key: "Access-Control-Allow-Methods", value: "POST, OPTIONS" },
           { key: "Access-Control-Allow-Headers", value: "Content-Type" },
         ],
