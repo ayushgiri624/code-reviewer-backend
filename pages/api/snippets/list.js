36fc2c370a039d447ba12af2066abc4fbd8736ce
Ayush <giriayush624@gmail.com>
2026-04-22 11:53:24 +0530

    Fix import paths for snippets

--- pages/api/snippets/list.js
+++ pages/api/snippets/list.js
@@ -1,5 +1,5 @@
-import { connectDB } from "../../lib/mongodb";
-import Snippet from "../../models/Snippet";
+import { connectDB } from "../../../lib/mongodb";
+import Snippet from "../../../models/Snippet";
 
 export default async function handler(req, res) {
   if (req.method !== "GET") {
